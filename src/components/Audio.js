69b05fd657460d9b64e4a639ce522e352aadef12
Breno Calazans <brenocalazans@gmail.com>
2020-05-03 16:17:32 -0300

    Add download instructions

--- src/components/Audio.js
+++ src/components/Audio.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useAmp } from './AmpContext'
+import DownloadIcon from './DownloadIcon'
+import styled from 'styled-components'
 
 const baseUrl = 'https://d3dabxdkbtmy0c.cloudfront.net/'
 
 const audioExtension = /.(mp4|mp3|ogg)/
+
+const Small = styled.span`
+  font-size: 0.5em;
+  color: #999;
+  width: 120px;
+  display: inline-block;
+  margin-left: 1em;
+`
 
 const Audio = ({ file }) => {
   const url = `${baseUrl}${file}`
   const trackFile = file.replace(audioExtension, '.vtt')
 
   const amp = useAmp()
+
+  const isSafari =
+    document && /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
+  const isTouch =
+    document && ('ontouchstart' in window || navigator.msMaxTouchPoints > 0)
 
   if (amp) {
     return (
@@ -20,18 +35,63 @@ const Audio = ({ file }) => {
           <div fallback="">
             <p>Your browser doesn’t support HTML5 audio</p>
           </div>
+          <a href={url} download>
+            Baixar
+          </a>
         </amp-audio>
       </>
     )
   }
 
   return (
-    <div>
-      <audio controls>
+    <div
+      css={css`
+        display: flex;
+        align-items: center;
+        @media (max-width: ${(props) => props.theme.breakpoints.phone}) {
+          display: block;
+        }
+      `}
+    >
+      <audio
+        controls
+        css={css`
+          width: 100%;
+        `}
+      >
         <source src={url} />
         {/* <track default src={`/vtt/${trackFile}`} srcLang="pt" /> */}
         <p>Seu navegador não suporta o elemento audio.</p>
       </audio>
+      {true && (
+        <a
+          href={url}
+          download
+          css={css`
+            margin-left: 0.5em;
+            display: flex;
+            align-items: center;
+            @media (max-width: ${(props) => props.theme.breakpoints.phone}) {
+              margin-top: 0.5em;
+              margin-left: 0;
+              margin-bottom: 1em;
+            }
+          `}
+        >
+          <DownloadIcon />
+          {isTouch ? (
+            isSafari ? (
+              <Small>Segure o botão e escolha 'Baixar arquivo vinculado'</Small>
+            ) : (
+              <Small>Segure o botão e escolha 'Fazer download do link'</Small>
+            )
+          ) : (
+            <Small>
+              Clique com o botão direito e escolha 'Salvar link como'
+            </Small>
+          )}
+        </a>
+      )}
     </div>
   )
 }
