2ece8dcce7ad6278dc4df8e621cecc57c44a2b8a
Breno Calazans <brenocalazans@gmail.com>
2020-05-08 12:32:33 -0300

    Muda texto pras pessoas nao acharem que é o botao de compartilhar o audio

--- src/components/Contribute.js
+++ src/components/Contribute.js
@@ -5,28 +5,29 @@ import { button } from './Button'
 
 const Image = styled.img`
   margin-right: 0.5em;
+  width: 30px;
+  height: 30px;
 `
 
 const Contribute = () => {
   return (
-    <div
-      css={css`
-        text-align: right;
-      `}
+    <a
+      css={
+        button +
+        css`
+          text-align: left;
+        `
+      }
+      href="https://wa.me/5521990609164?text=Oi%2C%20estou%20enviando%20um%20audio%20e%20sua%20transcri%C3%A7%C3%A3o%20pra%20colocar%20no%20site!%20Segue:"
     >
-      <a
-        css={button}
-        href="https://wa.me/5521990609164?text=Oi%2C%20estou%20enviando%20um%20audio%20e%20sua%20transcri%C3%A7%C3%A3o%20pra%20colocar%20no%20site!%20Segue:"
-      >
-        <Image
-          height={30}
-          width={30}
-          src="/whatsapp-logo.png"
-          alt="WhatsApp logo"
-        />
-        Enviar novo áudio
-      </a>
-    </div>
+      <Image
+        height={30}
+        width={30}
+        src="/whatsapp-logo.png"
+        alt="WhatsApp logo"
+      />
+      Enviar novo áudio pro site
+    </a>
   )
 }
 
